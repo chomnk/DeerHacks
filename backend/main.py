f90d3b3a3d2415aff746e00150e7b694eb2b8e63
chomnk <86968977+chomnk@users.noreply.github.com>
2024-02-17 16:53:37 -0500

    test

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,47 @@ import requests
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+import json
+import certifi
+from pymongo import MongoClient
 
+file_path = 'the_modified_garbage_item_file-2.json'
+
+
+with open(file_path, 'r') as file:
+    data = json.load(file)
+    
 app = Flask(__name__)
 CORS(app)
 
 api_key = os.getenv("API_KEY")
 print(api_key)
+
+@app.route('/report', methods=['GET', 'POST'])
+def handle_report():
+    if request.method == 'POST':
+        type = request.json["type"]
+        lat = request.json["lat"]
+        lon = request.json["lon"]
+        
+        
+    uri = "mongodb+srv://Deerhacks:deerhacks@deerhacks.9v30i1z.mongodb.net/"
+    client = MongoClient(uri, tlsCAFile=certifi.where())
+    db = client.sample_garbage_database
+    coll = db["sample_garbage"]
+    #myclient = pymongo.MongoClient("mongodb+srv://Deerhacks:deerhacks@deerhacks.9v30i1z.mongodb.net/")
+   # mydb = myclient["sample_garbage_database"]
+    #mycol = mydb["sample_garbage_collection"]
+    
+    mydict = {"type": type, "lat": lat, "lon": lon}
+    x = coll.insert_one(mydict)
+    
+    if x.inserted_id != None:
+        return "Success"
+    else:
+        return "Error"
+        
+        
 
 @app.route('/classify', methods=['GET', 'POST'])
 def handle_classify():
@@ -28,10 +63,7 @@ def handle_classify():
                     "content": [
                         {
                             "type": "text",
-                            "text": "familiar with this json file: 'https://ckan0.cf.opendata.inter.prod-toronto.ca" +
-                            "/dataset/76ea33f1-31f0-42ea-a295-385fb4d94ea9/resource/274fde37-233c-485d-bc68-8177f0793412/do" + 
-                            "wnload/Waste%20Wizard%20Data%20from%20TOwaste%20App.json'. What is the type of trash in the image? return the " + 
-                            "ID and instruction of best match."
+                            "text": f"familiar yourself with this JSON: {data}, and classify which type of garbage the image belongs to. return json containing 1 field: id"
                         },
                 {
                     "type": "image_url",
--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import axios from 'axios';
 import './Main.css'
-import Camera from './Camera';
 import Webcam from "react-webcam";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useCallback, useState, useRef } from "react"; // import useCallback
@@ -13,14 +12,72 @@ const Main = (props) => {
     const isAuthenticated = props.isAuthenticated;
     const isLoading = props.isLoading;
 
+    const [location, setLocation] = useState({ latitude: null, longitude: null });
+    const [GarbageType, setGarbageType] = useState("");
+    const [GarbageBin, setGarbageBin] = useState("");
+    const [items, setItems] = useState([]);
+
+    const [isScanDisabled, setIsScanDisabled] = useState(false);
+    const [isReportDisabled, setIsReportDisabled] = useState(true);
+    const [ScanButtonText, setScanButtonText] = useState("Scan")
+    const [DisplayText, setDisplayText] = useState("")
+
     const handleReportClick = () => {
+        // This takes a screenshot and store as an image
+        const imageSrc = webcamRef.current.getScreenshot();
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+            (err) => {
+                console.error(err.message);
+            }
+        );
+
+        try {
+            const response = axios.post("http://localhost:5001/report", 
+                {
+                    "lat": location.latitude,
+                    "lon": location.longitude,
+                    "type": GarbageType
+                }
+            );
+            response.then((res) => {
+                //append to scan history list
+                setItems([...items, GarbageType])
+                //
+                setIsScanDisabled(false);
+                setScanButtonText("Scan");
+                setIsReportDisabled(true);
+                setDisplayText("");
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+
+    const handleScanClick = () => {
         // This takes a screenshot and store as an image
         const imageSrc = webcamRef.current.getScreenshot();
 
         try {
             // This sends a POST request to the backend with the json containting the image in base64
-            const response = axios.post("http://localhost:5001/classify", {"ImageData": imageSrc.split(',')[1]});
-            response.then((res) => console.log(res.data));
+            const response = axios.post("http://localhost:5001/classify", 
+                {
+                    "ImageData": imageSrc.split(',')[1],
+                }
+            );
+            response.then((res) => {
+                setGarbageType(res.data.type);
+                setGarbageBin(res.data.bin);
+                setIsReportDisabled(true);
+                setScanButtonText("Cancel");
+                setDisplayText(`This is a ${GarbageType} garbage.`);
+            });
         } catch (error) {
             console.error(error);
         }
@@ -46,24 +103,27 @@ const Main = (props) => {
             <div className="home-container2">
                 <Webcam className="home-video" height={600} width={700} ref={webcamRef} />
             <div className="home-container3">
-                <button type="button" onClick={() => handleReportClick()} className="home-button button">Report</button>
                 <span className="home-text05">
-                This is a &#123;GarbageType&#125; garbage.
+                    {DisplayText}
                 </span>
             </div>
-            </div>
             <div className="home-container4">
+            <button type="button" onClick={() => handleScanClick()} className="home-button button">
+              {ScanButtonText}
+            </button>
+            <button type="button" onClick={() => handleReportClick()} className="home-button1 button">
+              Report
+            </button>
+          </div>
+            </div>
+            <div className="home-container5">
             <span className="home-text06">Your Scan History</span>
             <ul className="home-ul list">
-                <li className="list-item">
-                <span className="home-text07">Text</span>
-                </li>
-                <li className="list-item">
-                <span className="home-text08">Text</span>
-                </li>
-                <li className="list-item">
-                <span className="home-text09">Text</span>
-                </li>
+                {items.map((item, index) => (
+                    <li key={index} className="list-item">
+                        <span className="home-text07">{`You scanned a ${item}.`}</span>
+                    </li>
+                ))}
             </ul>
             </div>
         </div>
